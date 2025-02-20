6466229879d7febc7a51b4cbe44c699055dd4d9b
bayer <bbayerdev@gmail.com>
2025-02-19 23:50:08 -0300

    feat: redirect in form after submission

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,20 @@ import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { toast } from "@/hooks/use-toast"
 import { Toaster } from "@/components/ui/toaster"
+import { useState } from "react"
+import { LoaderCircle } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 export default function Home() {
+  const router = useRouter()
+
   const { register, handleSubmit, formState: { errors } } = useForm<UserNameFormData>({
     resolver: zodResolver(formSchema)
   })
 
+  const [loading, setLoading] = useState(false)
   const onSubmit = async (data: UserNameFormData) => {
+    setLoading(true)
     try {
       const res = await fetch('api/user', {
         method: 'POST',
@@ -29,9 +36,14 @@ export default function Home() {
           title: `Welcome! ${data.name} 🎉🎉`,
           duration: 2000,
         })
+
+        setTimeout(() =>{
+          router.push('/dashboard')
+        }, 2000)
       }
     }
     catch (erro) {
+      setLoading(false)
       console.error
     }
   }
@@ -39,12 +51,22 @@ export default function Home() {
   return (
     <main className="flex justify-center items-center h-screen ">
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
-        <Label htmlFor="name" className="font-bold">Name</Label>npm install @hookform/resolvers zod
+        <Label htmlFor="name" className="font-bold">Name</Label>
         <Input {...register('name')} type="text" id="name" placeholder="Type your name" />
         {errors.name && (<p className="text-red-500 text-sm">{errors.name.message}</p>)}
-
         <Label className="text-neutral-500">This is your public display name.</Label>
-        <Button type="submit" variant={'secondary'} className="w-min mt-3">Send</Button>
+
+        <Button type="submit" variant={'secondary'} className="w-1/3 mt-3">
+          {loading ? (
+            <>
+            <LoaderCircle className="animate-spin"/>
+            </>
+          ) : (
+            <>
+              Send
+            </>
+          )}
+        </Button>
       </form>
       <Toaster />
     </main>
